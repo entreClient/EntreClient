e82df6bc3cabc79bc6a24762208a454782b95686
Alyne Nayely Regalado Morales <alyn.remo@gmail.com>
2023-05-02 01:21:06 -0500

    Interfaz registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//
+import { RegistroComponent } from './components/registro/registro.component';
+
+const routes: Routes = [
+    {path:'', redirectTo: 'registro', pathMatch: 'full'},
+  { path: 'registro', component: RegistroComponent },
+ 
+  { path: '**', redirectTo:'registro'  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+  
+}
